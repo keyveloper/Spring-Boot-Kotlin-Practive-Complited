6102adefdc25324b8c0a742745994160c0faf9a0
omen <didehdrb21@naver.com>
2024-08-16 15:38:17 +0900

    kotlin sever start

---
+++ src/main/kotlin/com/example/bulletinkotlin/BulletinKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.bulletinkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BulletinKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<BulletinKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/bulletinkotlin/BulletinKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.bulletinkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BulletinKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
